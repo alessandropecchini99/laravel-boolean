208a7f2c0d7b08c7d9561b616c707e4509ebf79a
alessandropecchini99 <pecchini99@gmail.com>
2023-07-04 15:33:16 +0200

    add show

---
+++ resources/views/tasks/show.blade.php
@@ -0,0 +1,26 @@
+@extends('layouts.base')
+
+@section('contents')
+    
+    <div class="show">
+
+        <div class="image" style="width: 100px; aspect-ratio: 1 / 1;">
+            <img src="{{ $task->image }}" alt="{{ $task->title }}">
+        </div>
+
+        <div class="info">
+            <ul class="list-group" style="margin-bottom: 2em;">
+                <li class="list-group-item"><span>Title:</span> {{ $task->title }}</li>
+                <li class="list-group-item"><span>Details:</span> {!! $task->details !!}</li>
+                <li class="list-group-item"><span>Creation Date:</span> {{ $task->creation_date }}</li>
+                <li class="list-group-item"><span>Expire Date:</span> {{ $task->expired_date }}</li>
+                <li class="list-group-item"><span>Done:</span> {{ $task->done }}</li>
+                <li class="list-group-item"><span>Urgent:</span> {{ $task->urgent }}</li>
+            </ul>
+
+            <a class="btn btn-secondary" href="/tasks">Back</a>
+        </div>
+
+    </div>
+
+@endsection
