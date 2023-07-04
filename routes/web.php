11dacb27781fe4c7aa4337d5c61eddc5433b24e7
alessandropecchini99 <pecchini99@gmail.com>
2023-07-04 14:42:26 +0200

    fix route

--- routes/web.php
+++ routes/web.php
@@ -15,4 +15,4 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [PagesController::class, 'home'])->name('home');
 
-Route::resource('comics', TaskController::class);
+Route::resource('tasks', TaskController::class);
